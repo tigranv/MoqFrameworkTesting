d4cc604bd7e14d8cea7095214b6e47f375d57381
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-08-07 15:04:33 +0400

    Nuint test

---
+++ TestingMoqFramework/EmployeeTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmployeeTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmployeeTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("80e60f0e-ad0c-48e3-8759-9384ae7179a6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestingMoqFramework/EmployeeTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using TestingMoqFramework.Model;
+
+namespace EmployeeTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            //Arrange
+            var pfDetails = new EmpPfDetails(new EmpPersonalDetails());
+
+            //Act
+            var contrib = pfDetails.GetPfEmployerControlSofar(1);
+
+            //Assert
+
+            Assert.That(contrib, Is.EqualTo(3456), "Its not as expected");
+        }
+    }
+}
